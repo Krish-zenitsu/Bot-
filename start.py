47d2cf57b5cf90e7eca26470ec62b4b735bf22d7
SUBHxTREM <71129596+SUBHxTREM@users.noreply.github.com>
2021-07-31 18:46:37 +0530

    Update start.py

--- start.py
+++ start.py
@@ -3,8 +3,8 @@ import subprocess
 from logging import DEBUG, INFO, basicConfig, getLogger, warning
 basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
 LOGS = getLogger("Helper")
-os.system("git clone https://github.com/Krish-zenitsu/Bot-")
-os.chdir("Bot-")
+os.system("git clone https://github.com/Krish-zenitsu/Itachi")
+os.chdir("Itachi")
 process = subprocess.Popen(
         ["python3", "-m", "userbot"],
         stdout=subprocess.PIPE,
