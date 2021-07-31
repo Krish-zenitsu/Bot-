1d21f58be474adf0a1c29c093a6b6497c429c03a
Krish-zenitsu <87484451+Krish-zenitsu@users.noreply.github.com>
2021-07-31 15:39:51 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -27,10 +27,10 @@ global ghanti
 ghanti = borg.uid
 edit_time = 5
 """ =======================CONSTANTS====================== """
-file1 = "https://telegra.ph/file/6aa39732748ed7c319943.jpg"
-file2 = "https://telegra.ph/file/a6d72504bc09e71484a54.jpg"
-file3 = "https://telegra.ph/file/3cdbede1d5d85aa2d50fc.jpg"
-file4 = "https://telegra.ph/file/3dae01973943e8b28c931.jpg"
+file1 = "https://telegra.ph/file/c82b154be4090e31f7225.png"
+file2 = "https://telegra.ph/file/c82b154be4090e31f7225.png"
+file3 = "https://telegra.ph/file/c82b154be4090e31f7225.png"
+file4 = "https://telegra.ph/file/c82b154be4090e31f7225.png"
 """ =======================CONSTANTS====================== """
 
 
